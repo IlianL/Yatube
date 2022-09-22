0f1019cdc8a5f57f0683b8f05191c0a015c828ec
Ilian Lypota <ilianlypota@gmail.com>
2022-09-22 19:18:19 +0300

    removed debug toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +68,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -154,6 +152,4 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'yatube_cache'),
     }
 }
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
